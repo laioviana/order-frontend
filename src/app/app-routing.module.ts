f9544373f342bcaba0d7f8bad4c34c29a8ab1cab
Laio Dias <laio.dias@lunatech.nl>
2022-06-06 22:26:36 +0200

    Add project routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddOrderComponent } from './components/add-order/add-order.component';
+import { OrderListComponent } from './components/order-list/order-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'orders', pathMatch: 'full' },
+  { path: 'orders', component: OrderListComponent },
+  { path: 'add', component: AddOrderComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
